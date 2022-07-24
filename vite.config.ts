27bf5a6151a3ac434e35e0e12918969b0ee2da41
rswindells <ross.swindells@outlook.com>
2022-07-24 22:35:52 +0100

    Add vuetfiy-loader equivalent for  vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import legacy from "@vitejs/plugin-legacy";
 import vue2 from "@vitejs/plugin-vue2";
 import vue2Jsx from "@vitejs/plugin-vue2-jsx";
 
+// vuetify-loader equivalent for vite
+import { VuetifyResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,6 +18,9 @@ export default defineConfig({
       targets: ["ie >= 11"],
       additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
     }),
+    Components({
+      resolvers: [VuetifyResolver()]
+    })
   ],
   resolve: {
     alias: {
